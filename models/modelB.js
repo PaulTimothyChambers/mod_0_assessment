3c3cde13a08f9c8f3f63f2decbf77adc81b315d0
Paul Chambers <paultimothychambers@gmail.com>
2021-06-11 18:32:16 -0600

    Create developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developers = {
+  jobTitle: "Software Programmer",
+  jobDesc: "programming of software",
+  lastBreakInMinAgo: 45,
+  lastMealInMinAgo: 8,
+  isMasterOfCraft: false,
+  isLearning: true,
+  enjoys: ["learning", "walks with the wife and dog", "learning more"],
+  hobbies: ["composing music", "making friends", "growing character"]
+};
