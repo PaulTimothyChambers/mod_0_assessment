7cbe835b841f3fe894858bc53397de55b9fdb031
Paul Chambers <paultimothychambers@gmail.com>
2021-06-11 18:26:42 -0600

    Create dino object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaurs = {
+  name: "Diplodicus",
+  nickname: "Dippo",
+  heightInM: 50,
+  weightInKg: 26000,
+  isScary: false,
+  isBig: true,
+  eats: ["berries", "leaves", "bark"],
+  friendsWith: ["members of same species", "other herbivores"]
+};
