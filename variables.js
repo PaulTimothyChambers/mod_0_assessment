f20336f8c518f501bc2b06fff898f2151e38839e
Paul Chambers <paultimothychambers@gmail.com>
2021-06-11 18:19:14 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,15 @@
+var movie = "Minority Report"
+var isEntertaining = true
+
+movie = "Brick"
+
+var num4 = 8
+var num6 = 12
+
+var product = (num4 * num6)
+
+var favouriteFoods = ["Pizza", "Pizzzzza", "ZaZa"]
+
+favouriteFoods.shift()
+
+console.log(movie.length)
